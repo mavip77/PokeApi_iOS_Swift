113c9aa00fd8ac93c1cd3f752176d1390aa27c05
MIGUEL VILLAN <applausecd@outlook.com>
2020-11-14 10:16:47 -0500

    Added Initial project and compatibility for iOS versions below 13.0

---
+++ PokeApiTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApiTest
+//
+//  Created by mac on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
